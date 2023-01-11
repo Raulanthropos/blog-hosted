261a199143bcf19c87f513b30e5d485b26e2f8d9
Raulanthropos <ipsichias@yandex.com>
2023-01-11 17:45:41 +0200

    update

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -2,11 +2,30 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
+import {useState, useEffect} from 'react'
 
 const BlogList = (props) => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetching = async () => {
+      let response = await fetch(
+        "https://strive-blog-be-production-d83c.up.railway.app/blogs"
+      );
+      if (response.ok) {
+        const fetchedData = await response.json();
+        setNews(fetchedData);
+      } else {
+        console.log("error");
+      }
+    };
+    fetching();
+  }, []);
+
+  console.log(news);
   return (
     <Row>
-      {posts.map((post) => (
+      {news.map((post) => (
         <Col
           md={4}
           style={{
